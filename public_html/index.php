40f438b339c75cb0508c310cb46775cc34047962
Thiago da Silva <thiagodasilva@protonmail.com>
2018-12-19 06:20:11 -0200

    Improve the content a bit

--- public_html/index.php
+++ public_html/index.php
@@ -1,12 +1,18 @@
 <?php
 
 $content = <<<HTML
-<h2>Olá!</h2>
+<div class="row">
+  <div class="col-12 col-sm-6">
+    <h2>Olá!</h2>
 
-<p>Eu sou o Thiago. Eu tenho 20 anos, e faço desenvolvimento web. Eu trabalho com HTML5, CSS3, JavaScript ES6 e PHP 7.
+    <p>Eu sou o Thiago. Eu tenho 20 anos, e faço desenvolvimento web. Eu trabalho com HTML, CSS, bastante JavaScript e um pouco de PHP.</p>
 
-<p>No momento estou procurando um emprego nesta área. Se você está procurando um desenvolvedor apaixonado pela web e
-  aberto para aprender coisas novas, vamos conversar, entre em contato.
+    <p>Eu amo código aberto, e já corrigi alguns bugs no <a href="https://github.com/mozilla/pdf.js">PDF.js</a> da Mozilla.</p>
+
+    <p>No momento estou procurando um emprego nesta área. Se você está procurando um desenvolvedor apaixonado pela web e
+      aberto para aprender coisas novas, vamos conversar, entre em contato.</p>
+  </div>
+</div>
 
 <h2>Contato</h2>
 <address>
