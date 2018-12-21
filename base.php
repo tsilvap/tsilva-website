2d6529655425a5cfa5dab60fdec86252b0335f41
Thiago da Silva <thiagodasilva@protonmail.com>
2018-12-20 23:03:02 -0200

    Turn project into a Node module

--- base.php
+++ base.php
@@ -5,23 +5,22 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
 
-    <!-- CSS Bootstrap -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
-          integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous" />
-
-    <!-- Animate on Scroll -->
-    <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
-
-    <title>Thiago da Silva</title>
-
     <meta name="description" content="Site pessoal de um desenvolvedor web." />
     <meta name="keywords" content="desenvolvedor, desenvolvimento, web, pessoal" />
     <meta name="author" content="Thiago da Silva" />
 
-    <link rel="stylesheet" href="css/style.css" />
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css"
-          integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous" />
+    <!-- CSS Bootstrap -->
+    <link rel="stylesheet" href="css/bootstrap/bootstrap.min.css" />
+    <!-- Animate on Scroll -->
+    <link rel="stylesheet" href="css/aos/aos.css">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="css/font-awesome/all.min.css" />
+    <!-- Source Code Pro para o logo -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Code+Pro&amp;text=Thiago%20da%20Silva" />
+    <!-- Estilos próprios -->
+    <link rel="stylesheet" href="css/style.css" />
+
+    <title>Thiago da Silva</title>
   </head>
   <body>
     <div class="page-wrapper d-flex flex-column">
@@ -63,17 +62,18 @@
     </div>
 
     <!-- jQuery primeiro, depois Popper.js, depois JS Bootstrap -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <script src="js/jquery/jquery.slim.min.js"></script>
+    <script src="js/popper.js/popper.min.js"></script>
+    <script src="js/bootstrap/bootstrap.min.js"></script>
 
     <!-- Animate on Scroll -->
-    <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+    <script src="js/aos/aos.js"></script>
     <script>AOS.init({
       duration: 1000,
       once: true,
     });</script>
 
+    <!-- Scripts próprios -->
     <script src="js/script.js"></script>
   </body>
 </html>
